a1bc71dfdde5888a6464c610f0b4025cfca434d8
Tom Grayston <t.grayston@lancaster.ac.uk>
2020-08-29 17:58:43 +0100

    Comments

--- analyser.py
+++ analyser.py
@@ -6,11 +6,12 @@ from ipwhois.asn import IPASN
 from ipwhois.net import Net
 
 def genAnalysis():
-    filename = "mirai_data.json"        #input name of file
-    file = open(filename, 'r',encoding='utf-16')
-    dict = json.loads(file.read())
-    Results = dict['results']
-    df=pd.DataFrame.from_dict(Results)
+    filename = "mirai_data.json"                                    #input name of csv file
+    file = open(filename, 'r',encoding='utf-16')                    #open file
+    dict = json.loads(file.read())                                  #convert file to dictionary format
+    Results = dict['results']                                       #grab the named section of the original file you want
+    df=pd.DataFrame.from_dict(Results)                              #using the panda lib to get the data frame of the file 
+
                                                                     #   | All |
     S_IP = df['Source IP'].value_counts()                           # Source IP
     HP = df['Honeypot'].value_counts()                              # Honeypot
@@ -29,6 +30,7 @@ def genAnalysis():
     TCP_F15 = df['TCP Flag'].value_counts().head(15)                # TCP Flag Top 15
     TCP_Win_Size15 = df['TCP Window Size'].value_counts().head(15)  # TCP Window Size Top 15
 
+    #generate CSV's of resulst from the dataset
     S_IP.to_csv(r"SourceIP.csv", header=True)
     HP.to_csv(r"Honeypot.csv", header=True)
     S_P.to_csv(r"SourcePorts.csv",header=True)
@@ -37,6 +39,7 @@ def genAnalysis():
     TCP_F.to_csv(r"TCPFlags.csv",header=True)
     TCP_Win_Size.to_csv(r"TCPWindowSize.csv",header=True)
 
+    #generate top 15 of each CSV
     S_IP15.to_csv(r"SourceIP_15.csv", header=True)
     HP15.to_csv(r"Honeypot_15.csv", header=True)
     S_P15.to_csv(r"SourcePorts_15.csv",header=True)
@@ -45,21 +48,23 @@ def genAnalysis():
     TCP_F15.to_csv(r"TCPFlags_15.csv",header=True)
     TCP_Win_Size15.to_csv(r"TCPWindowSize_15.csv",header=True)
 
+    #generate top 1000 IP addresses CSV's
     df=pd.read_csv(r"SourceIP.csv").head(1000)
     df.to_csv(r"SourceIP_1000.csv")
 
 def geolocate():
-    df = pd.read_csv("SourceIP.csv")
-    x = 0
+    df = pd.read_csv("SourceIP.csv")                                                                    #read in data from the IP csv file
+    Counter = 0
     errorCounter = 0
-    data=pd.DataFrame(columns=['IP','asn_country_code'])
+    data=pd.DataFrame(columns=['IP','CountryCode'])                                                     #create new dataframe with the columns IP and Country Code
+
     for i, row in df.iterrows():
         try:
-            x+=1
-            print(x)
+            Counter+=1
+            print(Counter)
             ip=row['Source IP']
-            results = IPASN(Net(ip)).lookup()
-            data=data.append({'IP':ip,'country_code':results['asn_country_code']},ignore_index=True)
+            results = IPASN(Net(ip)).lookup()                                                           #Use the panda lib to search up the IP addresses from the file 
+            data=data.append({'IP':ip,'country_code':results['asn_country_code']},ignore_index=True)    #add the result of that search to the data
         except:
             print("Big Oof")
             errorCounter+=1
@@ -67,10 +72,10 @@ def geolocate():
     print("Error Amount")
     print(errorCounter)
 
+    #generate CSV's 
     data.to_csv(r"geolocatedAll.csv")
     df=pd.read_csv(r"geolocatedAll.csv")
     CC = df['country_code'].value_counts()
-
     CC.to_csv(r"CC_All.csv", header = True)
 
 geolocate()
